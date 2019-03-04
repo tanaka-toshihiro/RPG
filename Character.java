e3cc2b8bdfe1a478b65982fdce3534ddc8d458af
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-03-04 22:32:34 +0900

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,12 @@
+class Character{
+ public int hp;
+ public int mp;
+ public String name; 		
+ public String skill; 
+ public void introduce(){
+  System.out.println( name + "のステータス");
+  System.out.println("HP "+ hp);
+  System.out.println("MP "+ mp);
+  System.out.println("特殊技能:"+skill);	
+ }		
+}
