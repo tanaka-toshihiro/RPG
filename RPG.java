653933c052a9998c0383a559c976349d5b429ec7
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-03-13 00:04:48 +0900

    Update RPG.java

--- RPG.java
+++ RPG.java
@@ -1,25 +1,35 @@
 public class RPG{
-  public static void main (String[] args){	
+  public static void main (String[] args){
     Character Brave= new Character();
     Brave.hp = 500;
     Brave.mp = 500;
+    Brave.atk = 100;
     Brave.name = "勇者";
-    Brave.skill = "タンスを漁る";
-  	
+    Brave.skill = "シャイニング・ソード";
+    
     Character Mage = new Character();
     Mage.hp = 200;
     Mage.mp = 1000;
     Mage.name = "魔法使い";
+    Mage.atk = 120;
     Mage.skill = "ファイヤーボール";
-  
+    
     Character Satan = new Character();
     Satan.hp = 99999;
     Satan.mp = 99999;
+    Satan.atk = 200;
     Satan.name = "魔王";
     Satan.skill = "眷属召喚";
-  
+    
     Brave.introduce();
     Mage.introduce();
-    Satan.introduce();	 
-  }
+    Satan.introduce();
+    
+    System.out.println("バトル開始！");
+
+    Brave.attack(Satan);
+    Mage.attack(Satan);
+    Satan.attack(Brave);
+    Satan.attack(Mage);
+ }
 }
